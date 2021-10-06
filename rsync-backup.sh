ebd5520d8d7c035d92b16848aca273c45e66ea28
Michael "Sei" Reiley <seisatsu@users.noreply.github.com>
2021-10-06 10:29:48 -0700

    Update rsync-backup.sh

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 backupDirectories=("/var/www/somedir" "/var/lib/mysql")
+host=BACKUPHOST
+port=BACKUPPORT
 
 echo "Beginning backups for `date +%F`."
 
 for srcdir in ${backupDirectories[@]}; do
     echo "Backing up $srcdir..."
-    rsync -av -e "ssh -p PORT -i /root/.ssh/rsync" $srcdir rsync@HOST:/home/rsync/backup/`date +%F`/
+    rsync -av -e "ssh -p $port -i /root/.ssh/rsync" $srcdir rsync@$host:/home/rsync/backup/`date +%F`/
 done
 
 echo "Finished backups."
