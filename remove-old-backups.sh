e35700d3f6cb3ed492ec6d9edd8b84bb67ae9835
Michael "Sei" Reiley <seisatsu@users.noreply.github.com>
2021-10-06 10:25:54 -0700

    Create remove-old-backups.sh

---
+++ remove-old-backups.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+MAXDISKUSAGE=80
+
+df -H | grep -vE '^Filesystem|tmpfs|devtmpfs|none' | awk '{ print $5 " " $1 }' | while read output;
+do
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  files=`ls -d /home/rsync/backup`
+  if [ $usep -ge $MAXDISKUSAGE ]; then
+    echo "Removing old backup: ${files[0]}"
+    rm -rf ${files[0]}
+  fi
+done
